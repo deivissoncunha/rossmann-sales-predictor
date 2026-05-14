1fb694bfae2a55a8baeb22d7c76e82fd478ebf4c
Deivisson Cunha <deivissonlcunha@gmail.com>
2026-05-14 10:38:59 -0300

    add store dataset merge and clean repository

--- handler.py
+++ handler.py
@@ -1,16 +1,46 @@
 import os
 import pickle
 import pandas as pd
+
 from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
-# load model
+
+# ============================================================
+# Paths
+# ============================================================
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# model_path
 model_path = os.path.join(BASE_DIR, 'model', 'model_rossmann.pkl')
+
+# dataset path
+store_path = os.path.join(BASE_DIR, 'dataset', 'store.csv')
+
+# ============================================================
+# Load files
+# ============================================================
+
+# load model
 model = pickle.load(open(model_path, 'rb'))
 
+# load store dataset
+df_store_raw = pd.read_csv(store_path)
+
+# lowercase columns
+df_store_raw.columns = map(str.lower, df_store_raw.columns)
+
+# ============================================================
 # initialize API
+# ============================================================
+
 app = Flask(__name__)
+
+# ============================================================
+# Prediction Endpoint
+# ============================================================
 
 @app.route('/predict', methods=['POST'])
 def rossmann_predict():
@@ -21,19 +51,35 @@ def rossmann_predict():
             test_raw = pd.DataFrame(test_json, index=[0])
         else:
             test_raw = pd.DataFrame(test_json)
+        
+        # lowercase columns
+        test_raw.columns = map(str.lower, test_raw.columns)
 
+        # merge store dataset
+        test_raw = pd.merge(
+            test_raw,
+            df_store_raw,
+            how='left',
+            on='store'
+        )
+
+        # pipeline
         pipeline = Rossmann()
 
         df1 = pipeline.data_cleaning(test_raw)
         df2 = pipeline.feature_engineering(df1)
         df3 = pipeline.data_preparation(df2)
 
+        # prediction
         df_response = pipeline.get_prediction(model, test_raw, df3)
 
         return df_response
 
     return Response({}, status=200, mimetype='application/json')
 
+# =========================================================
+# Main
+# =========================================================7
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
